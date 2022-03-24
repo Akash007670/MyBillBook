f31312150470e8dbf74ff43b6b0abbf52a474386
Akash kumar <akaysharma894@gmail.com>
2022-03-25 01:18:03 +0530

    Added hero section with login form with validations and api

---
+++ src/Components/LoginSection/index.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import {
+  ContentWrapper,
+  Heading,
+  Heading2,
+  Heading3,
+  HeroText,
+  HeroTextWrapper,
+  InputAddon,
+  InputWrapper,
+  Isowrapper,
+  LoginFormWrapper,
+  LoginSectionWrapper,
+  MadeInLoveWrapper,
+  OtpInput,
+  OtpInputWrapper,
+  Span,
+  Button,
+  ErrorText,
+} from "./LoginElement";
+import IsoLogo from "../../assets/icn_ISO.svg";
+import axios from "axios";
+
+const LoginSection = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [error, setError] = useState("");
+
+  const isValidNumber = (phoneNumber) => {
+    const regex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
+    const result = phoneNumber.match(regex);
+    if (result) {
+      return true;
+    }
+    return false;
+  };
+
+  const getOtpHandler = async () => {
+    if (!phoneNumber && !isValidNumber(phoneNumber)) {
+      return setError("valid mobile number required!");
+    }
+    const body = { mobile_number: phoneNumber };
+    try {
+      const res = await axios.post(
+        "https://niobooks.in/api/web/request_otp",
+        body
+      );
+      console.log(res);
+    } catch {
+      console.log("error");
+    }
+  };
+
+  return (
+    <>
+      <LoginSectionWrapper>
+        <ContentWrapper>
+          <HeroTextWrapper>
+            <HeroText>
+              <Heading>Simple GST Billing &#38; Stock Management</Heading>
+              <Heading2>software for your business</Heading2>
+              <Heading3>Atma Nirbhar Vyapaari bane</Heading3>
+            </HeroText>
+            <MadeInLoveWrapper>
+              <Heading3>Made with &#128151; in India</Heading3>
+              <Isowrapper>
+                <img src={IsoLogo} alt="isologo" />
+              </Isowrapper>
+            </MadeInLoveWrapper>
+          </HeroTextWrapper>
+          {/* //Login part starts here // */}
+          <LoginFormWrapper>
+            <Heading3>Login To myBillBook</Heading3>
+            <InputWrapper>
+              <Span>Enter the Mobile Number</Span>
+              <OtpInputWrapper>
+                <InputAddon>+91</InputAddon>
+                <OtpInput
+                  input
+                  type="number"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              </OtpInputWrapper>
+              {getOtpHandler ? <ErrorText>{error}</ErrorText> : ""}
+              <Button
+                onClick={getOtpHandler}
+                hasPhoneNumber={phoneNumber.length > 9}
+              >
+                GET OTP
+              </Button>
+            </InputWrapper>
+          </LoginFormWrapper>
+        </ContentWrapper>
+      </LoginSectionWrapper>
+    </>
+  );
+};
+
+export default LoginSection;
