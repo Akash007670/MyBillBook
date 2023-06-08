c9cfb9c4adcb4f7d4460c172a0c16fa3f7256afe
Akash kumar <akaysharma894@gmail.com>
2023-06-08 14:33:12 +0530

    updated react version & items dahsboard

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-ReactDOM.render(
+import AuthProvider from "./Components/AuthContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <Router>
+    <AuthProvider>
       <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+    </AuthProvider>
+  </React.StrictMode>
 );
